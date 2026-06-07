c2208b06808156d26825069e1cda4b7fc873edc4
elenaportuguez999 <elenaportuguez999@users.noreply.github.com>
2026-06-07 12:44:15 +0200

    MonoBehaviour

---
+++ Assets/ModernSupermarket/Scripts/SimpleFPSController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFPSController : MonoBehaviour
+{
+    public float walkSpeed = 4f;
+    public float mouseSensitivity = 2f;
+    public float gravity = -9.81f;
+
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float cameraPitch;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        if (cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * walkSpeed * Time.deltaTime);
+
+        if (controller.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f;
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+        controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
+    }
+}
