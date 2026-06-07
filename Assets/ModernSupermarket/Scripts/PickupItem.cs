540756ef8f283d0828df8676e476c17463a20792
elenaportuguez999 <elenaportuguez999@users.noreply.github.com>
2026-06-07 13:31:53 +0200

    PickupItem

---
+++ Assets/ModernSupermarket/Scripts/PickupItem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    public Transform holdPoint;
+    public float pickupDistance = 3f;
+
+    private GameObject heldItem;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldItem == null)
+                TryPickup();
+            else
+                DropItem();
+        }
+    }
+
+    void TryPickup()
+    {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, pickupDistance))
+        {
+            if (hit.collider.CompareTag("Product"))
+            {
+                heldItem = hit.collider.gameObject;
+
+                Rigidbody rb = heldItem.GetComponent<Rigidbody>();
+                if (rb != null) rb.isKinematic = true;
+
+                heldItem.transform.SetParent(holdPoint);
+                heldItem.transform.localPosition = Vector3.zero;
+                heldItem.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+
+    void DropItem()
+    {
+        heldItem.transform.SetParent(null);
+
+        Rigidbody rb = heldItem.GetComponent<Rigidbody>();
+        if (rb != null) rb.isKinematic = false;
+
+        heldItem = null;
+    }
+
+    public bool HasItem()
+    {
+        return heldItem != null;
+    }
+
+    public void RemoveHeldItem()
+    {
+        if (heldItem != null)
+        {
+            Destroy(heldItem);
+            heldItem = null;
+        }
+    }
+}
