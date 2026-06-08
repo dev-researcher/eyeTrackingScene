0b608539af30650ab09581b403416a76c61835e0
elenaportuguez999 <elenaportuguez999@users.noreply.github.com>
2026-06-08 11:11:46 +0200

    transform.position

--- Assets/ModernSupermarket/Scripts/SimpleFPSController.cs
+++ Assets/ModernSupermarket/Scripts/SimpleFPSController.cs
@@ -125,22 +125,23 @@ public class SimpleFPSController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = transform.position + (-transform.right * movementSpeed * Time.deltaTime);
+            transform.position = transform.position + (-transform.forward * movementSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = transform.position + (transform.right * movementSpeed * Time.deltaTime);
+            
+            transform.position = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
+            transform.position = transform.position + (transform.right * movementSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position = transform.position + (-transform.forward * movementSpeed * Time.deltaTime);
+            transform.position = transform.position + (-transform.right * movementSpeed * Time.deltaTime);
         }
 
         // if (Input.GetKey(KeyCode.Q))
