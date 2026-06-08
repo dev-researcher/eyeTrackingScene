8ba0e58675aa7845d1cf2324b842514905c5bc9a
elenaportuguez999 <elenaportuguez999@users.noreply.github.com>
2026-06-08 11:52:58 +0200

    TryPickup-notWorking

--- Assets/ModernSupermarket/Scripts/SimpleFPSController.cs
+++ Assets/ModernSupermarket/Scripts/SimpleFPSController.cs
@@ -1,103 +1,4 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class SimpleFPSController : MonoBehaviour
-// {
-//     public float walkSpeed = 4f;
-//     public float mouseSensitivity = 2f;
-//     public float gravity = -9.81f;
-
-//     public Transform cameraTransform;
-
-//     private CharacterController controller;
-//     private float verticalVelocity;
-//     private float cameraPitch;
-
-//     void Start()
-//     {
-//         controller = GetComponent<CharacterController>();
-
-//         if (cameraTransform == null)
-//         {
-//             cameraTransform = Camera.main.transform;
-//         }
-
-//         Cursor.lockState = CursorLockMode.Locked;
-//         Cursor.visible = false;
-//     }
-
-//     void Update()
-//     {
-//         // float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-//         // float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
-//         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-//         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-//         // Flechas izquierda/derecha para girar
-//         float keyboardLookSpeed = 0.7f;
-
-//         if (Input.GetKey(KeyCode.LeftArrow))
-//         {
-//             mouseX = -keyboardLookSpeed;
-//         }
-
-//         if (Input.GetKey(KeyCode.RightArrow))
-//         {
-//             mouseX = keyboardLookSpeed;
-//         }
-
-//         if (Input.GetKey(KeyCode.UpArrow))
-//         {
-//             mouseY = -keyboardLookSpeed;
-//         }
-
-//         if (Input.GetKey(KeyCode.DownArrow))
-//         {
-//             mouseY = keyboardLookSpeed;
-//         }
-
-//         transform.Rotate(Vector3.up * mouseX);
-
-//         cameraPitch -= mouseY;
-//         cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
-//         cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
-//         // h
-//         // float x = Input.GetAxis("Horizontal");
-//         // float z = Input.GetAxis("Vertical");
-//         // h
-
-//         float x = 0f;
-//         float z = 0f;
-
-//         if (Input.GetKey(KeyCode.A)) x = -1f;
-//         if (Input.GetKey(KeyCode.D)) x = 1f;
-//         if (Input.GetKey(KeyCode.W)) z = 1f;
-//         if (Input.GetKey(KeyCode.S)) z = -1f;
-
-//         // Vector3 move = transform.right * x + transform.forward * z;
-//         // controller.Move(move * walkSpeed * Time.deltaTime);
-
-//         // if (controller.isGrounded && verticalVelocity < 0)
-//         // {
-//         //     verticalVelocity = -2f;
-//         // }
-
-//         Vector3 move = transform.right * x + transform.forward * z;
-
-//         if (move.magnitude > 1f)
-//         {
-//             move.Normalize();
-//         }
-
-//         controller.Move(move * walkSpeed * Time.deltaTime);
-
-//         verticalVelocity += gravity * Time.deltaTime;
-//         controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
-//     }
-// }
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -176,34 +77,13 @@ public class SimpleFPSController : MonoBehaviour
         Vector3 angles = transform.localEulerAngles;
         transform.localEulerAngles = new Vector3(verticalRotation, angles.y, 0f);
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (heldItem == null)
-                TryPickup();
-            else
-                DropItem();
-        }
-
-        // if (Input.GetKey(KeyCode.Q))
+        // if (Input.GetKeyDown(KeyCode.E))
         // {
-        //     transform.position = transform.position + (transform.up * movementSpeed * Time.deltaTime);
+        //     if (heldItem == null)
+        //         TryPickup();
+        //     else
+        //         DropItem();
         // }
-
-        // if (Input.GetKey(KeyCode.E))
-        // {
-        //     transform.position = transform.position + (-transform.up * movementSpeed * Time.deltaTime);
-        // }
-
-        // if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp))
-        // {
-        //     transform.position = transform.position + (Vector3.up * movementSpeed * Time.deltaTime);
-        // }
-
-        // if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown))
-        // {
-        //     transform.position = transform.position + (-Vector3.up * movementSpeed * Time.deltaTime);
-        // }
-
 
         if (looking)
         {
